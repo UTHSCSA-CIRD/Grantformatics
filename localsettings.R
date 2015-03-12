518a95d1eedc8f909a88db4e57a4c468cf07a0d8
Sergio Duncan <sergioduncan@yahoo.com>
2015-03-12 11:42:38 -0500

    my own branch

--- localsettings.R
+++ localsettings.R
@@ -1,12 +1,12 @@
 ## Sergio settings
-# pfa_label <- c("disparities","dissemination", "methods","options", "systems") # where each fileid == category label
-# pathname <-  "C:\\Users\\sergan\\Documents\\barepcori" #enter path of abstracts
-# sepchar <- "\\"
+pfa_label <- c("disparities","dissemination", "methods","options", "systems") # where each fileid == category label
+pathname <-  "C:\\Users\\sergan\\Documents\\barepcori" #enter path of abstracts
+sepchar <- "\\"
 
 ## Alex settings
-pfa_label <- c("disp","diss","meth","options","systems");
-pathname <-  "."; #enter path of abstracts (I keep mine in the local dir)
-sepchar <- "/";
+#pfa_label <- c("disp","diss","meth","options","systems");
+#pathname <-  "."; #enter path of abstracts (I keep mine in the local dir)
+#sepchar <- "/";
 
 ## Common settings
 enc <- 'UTF-8';

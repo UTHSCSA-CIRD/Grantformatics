4763ad9184a780e31504e609d0820ace05927c06
root <root@localhost>
2015-03-13 03:06:45 -0500

    Okay, it pretty much works end-to-end now. And the PCORI the precision and recall turned out to suck especially for dissemination.

--- grantsifier.R
+++ grantsifier.R
@@ -3,20 +3,22 @@ libs <-c("RTextTools","plyr", "class", "tm");
 lapply(libs, require, character.only = TRUE);
 
 #Set options
-options(stringsAsFactors = FALSE)
+#options(stringsAsFactors = FALSE);
 
 #Set parameters
-source("localsettings.R")
+source("localsettings.R");
+# the built-in one seems to be broken
+source("create_analytics.R");
 
 # clean text-scrub out nasty html
 # except that we don't use stopwords for, you know, html...
 # maybe we should find/make a stopwords file for that. 
 # Also, maybe consider using the SMART set instead of english
 cleanCorpus <- function(corpus) {
-  corpus.tmp <- tm_map(corpus, removePunctuation)
-  corpus.tmp <- tm_map(corpus.tmp, stripWhitespace)
-  corpus.tmp <- tm_map(corpus.tmp, content_transformer(tolower))
-  corpus.tmp <- tm_map(corpus.tmp, removeWords, stopwords("english"))
+  corpus.tmp <- tm_map(corpus, removePunctuation);
+  corpus.tmp <- tm_map(corpus.tmp, stripWhitespace);
+  corpus.tmp <- tm_map(corpus.tmp, content_transformer(tolower));
+  corpus.tmp <- tm_map(corpus.tmp, removeWords, stopwords("SMART"));
   return(corpus.tmp)
 }
 
@@ -43,11 +45,24 @@ corpus <- cleanCorpus(corpus);
 
 # create a document term matrix
 # The control list is something we can play with and see how it affects results
-dm <- DocumentTermMatrix(corpus,control=list(removeNumbers=T));
+dm <- DocumentTermMatrix(corpus,control=list(removeNumbers=T,stemWords=T));
 
 # Now we use the pfa labels we created earlier to create the correct classes
 # Told you the lc variable (length of the corpus, i.e. number of documents) 
 # would come in handy. And no, not a virgin, not yet. Wait, what?
-dc <- create_container(dm,meta(corpus,"pfa")[,1],trainSize = 1:round(lc/2),virgin=F);
+# Note that we convert the second argument to numbers-- the labels must be numeric
+# otherwise certain columns come out as all-NA and there are errors and warnings.
+dc <- create_container(dm,as.numeric(factor(meta(corpus,"pfa")[,1])),trainSize = tsz <- 1:round(lc/2),testSize = seq(lc)[-tsz],virgin=F);
 
-trained <- train_models(dc,c("SVM","GLMNET","MAXENT"));
+# these three algorithms are the fast ones, that wont lock up your computer
+trained2 <- train_models(dc,c("SVM","GLMNET","MAXENT"));
+classed2 <- classify_models(dc,trained2);
+anals <- create_analytics(dc,classed2);
+print(summary(anals));
+consensus_labels<-factor(create_scoreSummary(dc,classed2)$BEST_PROB,levels=levels(dc@testing_codes));
+confmatrix<-addmargins(table(Real=dc@testing_codes,Predicted=consensus_labels));
+print(confmatrix);
+cat('Recall\n')
+print(diag(confmatrix)/confmatrix[,'Sum']);
+cat('Precision\n')
+print(diag(confmatrix)/confmatrix['Sum',]);
